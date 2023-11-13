821823ef1321e597f02619b6caaa60ce4b6d9731
Chris Williams <149628668+chris-williams-icann@users.noreply.github.com>
2023-11-13 05:08:57 -0800

    Update fileUtils.groovy

--- vars/fileUtils.groovy
+++ vars/fileUtils.groovy
@@ -1,4 +1,4 @@
-package org.christest
+urn package org.christest
 
 import java.io.File
 
@@ -8,12 +8,33 @@ def getFilesMatchingPattern(globPattern) {
 }
 
 def sortFilesByLastModified(files) {
-    files.sort { a, b ->
+    // Convert file paths to File objects
+    def fileObjects = files.collect { new File(it) }
+
+    // Sort files by last modified date
+    def sortedFiles = fileObjects.sort { a, b ->
+        long aLastModified = a.lastModified()
+        long bLastModified = b.lastModified()
+
+        // Compare last modified dates in descending order
+        bLastModified <=> aLastModified
+    }
+
+    // Convert File objects back to file paths
+    def sortedFilePaths = sortedFiles.collect { it.toString() }
+
+    return sortedFilePaths
+}
+
+def sortFilesObjsByLastModified(files) {
+    def sortedFiles = files.sort { a, b ->
         def aModified = a.getLastModified()
         def bModified = b.getLastModified()
+        println "a: ${a} aModified=${aModified}"
+        println "b: ${b} bModified=${bModified}"
         return bModified <=> aModified
     }
-    return files
+    return sortedFiles
 }
 
 def getZipFiles(antExpression) {
