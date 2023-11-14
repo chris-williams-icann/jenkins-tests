3745d4572386e40b0caf3f098ea26f7b90538ddf
Chris Williams <149628668+chris-williams-icann@users.noreply.github.com>
2023-11-14 05:02:32 -0800

    Update fileUtils.groovy

--- vars/fileUtils.groovy
+++ vars/fileUtils.groovy
@@ -1,20 +1,85 @@
 package org.christest
 
 import java.io.File
+import org.jenkinsci.plugins.pipeline.utility.steps.fs.FileWrapper
+
+def gitDiffAndFilterSortFiles(pattern) {
+    // Step 1: Get the list of changed files using git diff
+    def gitDiffCommand = 'git diff --name-only'
+    def changedFiles = sh(script: gitDiffCommand, returnStdout: true).trim().split('\n')
+
+    // Step 2: Filter files based on the provided pattern
+    def filteredFiles = changedFiles.findAll { file ->
+        file =~ pattern
+    }
+
+    // Step 3: Sort the filtered files by file name in descending order
+    def sortedFiles = filteredFiles.sort { a, b ->
+        b <=> a
+    }
+
+    // Step 4: Print the sorted file names (for demonstration purposes)
+    sortedFiles.each { file ->
+        echo "Sorted File: ${file}"
+    }
+
+    // Return the sorted file list (optional)
+    return sortedFiles
+}
 
 def getFilesMatchingPattern(globPattern) {
     def files = findFiles(glob: globPattern)
     return files
 }
 
-def sortFilesByLastModified(files) {
+def fileObjsToPaths(fileObjs) {
+    def result = []
+    def wrap = findFiles(glob: '*')
+    wrap.each { it, i ->
+      def full_path = it.getPath()
+      result.add(full_path)
+    }
+    return result
+}
+
+def getLastModified(fileObjs) {
+    if (!fileObjs instanceof File[]) {
+        throw new IllegalArgumentException("The paths fileObjs must be an array of File objects")
+    }
+    if (fileObjs.size() < 1) {
+        return null
+    }
+    def lastMod = fileObjs[0]
+    for (file in fileObjs) {
+        if (file.lastModified > lastMod.lastModified) {
+            lastMod = file
+        }
+    }
+    return lastMod
+}
+
+def pathsToFileObjects(paths) {
+    if (!paths instanceof String[]) {
+        throw new IllegalArgumentException("The paths parameter must be an array of strings")
+    }
+    def fileObjects = [] as List<File>
+    for (path in paths) {
+        fileObjects << new File(path)
+    }
+    return fileObjects
+}
+
+def sortFilesByLastModified(paths) {
+    if (!paths instanceof String[]) {
+        throw new IllegalArgumentException("The paths parameter must be an array of strings")
+    }
     // Convert file paths to File objects
-    def fileObjects = files.collect { new File(it) }
+    def fileObjects = paths.collect { new File(it) }
 
     // Sort files by last modified date
     def sortedFiles = fileObjects.sort { a, b ->
-        long aLastModified = a.lastModified()
-        long bLastModified = b.lastModified()
+        long aLastModified = a.lastModified
+        long bLastModified = b.lastModified
 
         // Compare last modified dates in descending order
         bLastModified <=> aLastModified
@@ -26,10 +91,13 @@ def sortFilesByLastModified(files) {
     return sortedFilePaths
 }
 
-def sortFilesObjsByLastModified(files) {
-    def sortedFiles = files.sort { a, b ->
-        def aModified = a.getLastModified()
-        def bModified = b.getLastModified()
+def sortFilesObjsByLastModified(fileObjs) {
+    if (!fileObjs instanceof File[]) {
+        throw new IllegalArgumentException("The paths fileObjs must be an array of File objects")
+    }
+    def sortedFiles = fileObjs.sort { a, b ->
+        def aModified = a.lastModified
+        def bModified = b.lastModified
         println "a: ${a} aModified=${aModified}"
         println "b: ${b} bModified=${bModified}"
         return bModified <=> aModified
